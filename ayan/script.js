78c4525cc7c6600e7cb744ab5a37731ca6f991fe
ayan khan <ayankhanayan992@gmail.com>
2026-03-25 23:30:55 +0530

    my website

--- ayan/script.js
+++ ayan/script.js
@@ -2,3 +2,33 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var elemc = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-images")
+elemc.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+})
+elemc.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+})
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+
+        var img = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${img})`
+    })
+})
+
+gsap.from("#footer-text h1", {
+    y: 150,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1,
+    scrollTrigger: {
+        trigger: "#footer",
+        scroller: "#main",
+        start: "top 70%"
+    }
+})
