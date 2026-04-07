0f96a63d0527e98f7b870d7264bb8c363a2fe705
ayan khan <ayankhanayan992@gmail.com>
2026-04-07 23:52:56 +0530

    landing page completed

--- DOM-PROJECT-COHORT/script.js
+++ DOM-PROJECT-COHORT/script.js
@@ -1,8 +1,11 @@
 var allElems= document.querySelectorAll('.elem')
+var allFullElems = document.querySelectorAll('.fullElem')
 
 allElems.forEach(function(elem){
     elem.addEventListener('click',function(){
-        console.log(elem.id)
+      /*   console.log(elem.id) */
+      allFullElems[elem.id].style.display='block'
+      
     })
 
 })
