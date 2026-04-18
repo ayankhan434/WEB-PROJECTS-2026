5818585d2c503b4689047692744b2e91902bcba5
ayan khan <ayankhanayan992@gmail.com>
2026-04-18 18:21:16 +0530

    to do almost completed

--- DOM-PROJECT-COHORT/script.js
+++ DOM-PROJECT-COHORT/script.js
@@ -24,9 +24,78 @@ allFullElemsBackBtn.forEach(function(back){
 openfeatures()
 
 let form = document.querySelector('.addtask form')
+let taskInput = document.querySelector('.addtask form #task-input')
+let taskdetailsInput = document.querySelector('.addtask form textarea')
+let taskCheckbox = document.querySelector('.addtask form #check')
 
-console.log(form)
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    console.log('form submitted')
+let currentTask=[
+    {
+        task:'coading',
+        details:'details of coading',
+        imp:false
+    },
+   {
+        task:'engineering maths',
+        details:'details of engineering maths',
+        imp:false
+   },
+   {
+        task:'revision',
+        details:'math and coading revision',
+        imp:true
+   }
+]
+
+
+ 
+ 
+
+
+function renderTask(){  
+var allTask = document.querySelector('.alltask')
+
+var sum= ''
+
+currentTask.forEach(function(elem){
+    sum+=`  <div class="task">
+              
+         
+              <h5>${elem.task}<span class=${elem.imp ? 'true' : 'false'}>imp</span></h5>
+              <button>mark as completed</button>
+           
+           
+            
+
+          </div>`
+    // console.log(elem)
 })
+
+// console.log(sum);
+
+allTask.innerHTML = sum 
+}
+renderTask()
+
+
+
+ console.log(form)
+ form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+     //console.log('form submitted')
+    console.log(taskInput.value)
+    console.log(taskdetailsInput.value)
+    console.log(taskCheckbox.checked) 
+
+    currentTask.push({
+        task:taskInput.value,
+        details:taskdetailsInput.value,
+        imp:taskCheckbox.checked
+    })
+    taskInput.value=''
+    taskdetailsInput.value=''
+    taskCheckbox.checked=false
+ // console.log(currentTask)
+  renderTask()
+} ) 
