c3b6bce42f96adae703995103a365f2a2b69420a
ayan khan <ayankhanayan992@gmail.com>
2026-07-05 22:09:15 +0530

    project day -02

--- SHOP-ME.COM/backend/routes/authRoutes.js
+++ SHOP-ME.COM/backend/routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const router=express.Router();
 const{registerUser,loginUser,getMe}=require('../controllers/authController.js');
+const{protect}=require('../middleware/authMiddleware.js');
+const admin=require('../middleware/adminMiddleware.js');
+
+
 
 router.post('/register',registerUser);
 router.post('/login',loginUser);
-router.get('/user',logoutUser);
+router.get('/users',protect,getUsers);
